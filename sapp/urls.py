8c3abed26e0dbd43943eaa65d9d26d47abe4a3c6
DESKTOP-N38FM86\HORLPC <promaxcomputers@gmail.com>
2022-09-06 20:31:07 +0000

    Modularized the pages into components

--- sapp/urls.py
+++ sapp/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 #MVT (Model Views Template)
+app_name = 'sapp'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('blog', views.blog, name='blog'),
+    path('about', views.about, name='about'),
+    path('support', views.support, name='support'),
+    path('contact', views.contact, name='contact'),
 ]
--- sapp/views.py
+++ sapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def support(request):
+    return render(request, 'support.html')
